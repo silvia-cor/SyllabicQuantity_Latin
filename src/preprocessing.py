cd79a9d59dc85b87cc1172b7442e70d3c81f2f70
Canapone98 <62448870+Canapone98@users.noreply.github.com>
2020-11-12 17:21:43 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,30 @@
 import re
-
-# ? studio xml tags
-# - togliere le cose da togliere (header, citazioni, tags..)
+#import nltk
+#from nltk import sent_tokenize
+#import string
 # - divisione dei testi per frasi
 # - togliere punteggiatura
-#
-#
-# def divide_fragments(path_file):
+
+def removeTags(path_file):
+	text=open(path_file,"r").read()
+	text_r = re.sub('<META(.*)>(\n.*)*<\/teiHeader>|<head(.*)>(.*)<\/head>|<app(.*)>(.*)<\/app>|<foreign(.*)>(.*)<\/foreign>|<quote(.*)>(.*)<\/quote>|<argument().*>(.*\n)*<\/p>|<note(.*)>(.*)<\/note>|<rf(.*)>(.*)<\/rf>|<i(.*)>(.*)<\/i>|<[^<]+>', "", text)
+	with open(path_file, "w") as f:
+		f.write(text_r)
+
+  		
+#def divide_sent(path_file):
+#	text = open(path_file,"r").read()
+#	sent = nltk.sent_tokenize(text)
+
+
+#def removePunct(path_file):
+	#text = open(path_file, "r").read()
+	#text_p = "".join([char for char in text if char not in string.punctuation])
+	#with open(path_file,"w") as f:
+		#f.write(text_p)
+	
+
+    # def divide_fragments(path_file):
 #     text = open(path_file, "r").read()
 #     text = text.replace("\n", " ")
 #     fragments = re.findall(r'<div2(.*?)*>(.*?)<\/div2>', text)
